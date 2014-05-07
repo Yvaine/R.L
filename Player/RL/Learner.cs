d83391e8928da6a8b1ab4c40735847e53601f444
dariushha <b.g.dariush+repo@gmail.com>
2014-05-07 11:42:16 +0430

    @ Player/RL/Learner.cs:  update rule CHANGED to : Q(gameState) = 500 * getReward(gameState) / gameState.GameStep + GAMMA * candidate.Key.Key

--- Player/RL/Learner.cs
+++ Player/RL/Learner.cs
@@ -123,7 +123,7 @@ namespace Player.RL
                 // update the mution factore for current state
                 updateMutaionFactore(gameState, candidate.Value);
                 // the New Q Value
-                var nQv = getReward(gameState) + GAMMA * candidate.Key.Key;
+                var nQv = 500 * getReward(gameState) / gameState.GameStep + GAMMA * candidate.Key.Key;
                 // update the Q value
                 updateQ(
                     gameState,          // The state
